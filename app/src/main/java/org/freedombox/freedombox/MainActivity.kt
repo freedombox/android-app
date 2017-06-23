3c622874dd8231263fb08df393a0fa0d23f4c6f4
Joseph Nuthalpati <njoseph@thoughtworks.com>
2017-06-23 19:05:57 +0530

    Updated to the latest version of apk
    - Reconstructed sources from the decompiled FreedomBox for villages app.

--- app/src/main/java/org/freedombox/freedombox/MainActivity.kt
+++ app/src/main/java/org/freedombox/freedombox/MainActivity.kt
@@ -5,19 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
-
     private var mWebView: WebView? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(paramBundle: Bundle?) {
+        super.onCreate(paramBundle)
         setContentView(R.layout.activity_main)
-
-        mWebView = findViewById(R.id.activity_main_webview) as WebView
-        mWebView!!.addJavascriptInterface(WebAppInterface(this), "Android")
-
-
-        val webSettings = mWebView!!.settings
-        webSettings.javaScriptEnabled = true
-        mWebView!!.loadUrl("file:///android_asset/index.html")
+        this.mWebView = findViewById(R.id.activity_main_webview) as WebView
+        this.mWebView!!.addJavascriptInterface(WebAppInterface(this), "Android")
+        this.mWebView!!.settings.javaScriptEnabled = true
+        this.mWebView!!.loadUrl("file:///android_asset/index.html")
     }
 }
--- app/src/main/java/org/freedombox/freedombox/WebAppInterface.kt
+++ app/src/main/java/org/freedombox/freedombox/WebAppInterface.kt
@@ -3,31 +3,41 @@ package org.freedombox.freedombox
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.pm.PackageManager.NameNotFoundException
+import android.net.Uri
 import android.util.Log
 import android.webkit.JavascriptInterface
+import kotlin.collections.List
+import kotlin.collections.HashMap
 
-import java.util.HashMap
-
-class WebAppInterface internal constructor(internal var context: Context) {
-    internal var packageName: HashMap<String, String>
+class WebAppInterface internal constructor(context: Context) {
+    internal var context = context
+    internal var packages = HashMap<String, List<String>>()
 
     init {
-        packageName = HashMap<String, String>()
-        packageName.put("sip", "com.csipsimple")
-        packageName.put("vlc", "org.videolan.vlc")
+        this.packages.put("sip", listOf("com.csipsimple"))
+        this.packages.put("vlc", listOf("org.videolan.vlc"))
+        this.packages.put("ampache", listOf("com.sound.ampache", "com.antoniotari.reactiveampacheapp"))
+        this.packages.put("storage", listOf("com.nextcloud.client", "com.ocloud24.android"))
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://"))
+        val resolveInfo = this.context.packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+        this.packages.put("browser", listOf(resolveInfo.resolvePackageName))
     }
 
     @JavascriptInterface
-    fun launchApp(app: String) {
-        val manager = context.packageManager
-
+    fun launchApp(appName: String) {
+        val packageManager = this.context.packageManager
+        val packageCandidates = this.packages[appName]
         try {
-            val launchIntent = manager.getLaunchIntentForPackage(packageName[app]) ?: throw PackageManager.NameNotFoundException()
-            launchIntent.addCategory(Intent.CATEGORY_LAUNCHER)
-            context.startActivity(launchIntent)
-        } catch (e: PackageManager.NameNotFoundException) {
-            Log.i("ERROR:", app + " is not installed")
+            val selectedPackage = packageCandidates!!.firstOrNull { candidate ->
+                packageManager.getLaunchIntentForPackage(candidate) != null
+            } ?: throw NameNotFoundException()
+            val intent = packageManager.getLaunchIntentForPackage(selectedPackage)
+            intent.addCategory("android.intent.category.LAUNCHER")
+            this.context.startActivity(intent)
+        } catch (ex: NameNotFoundException) {
+            Log.i("ERROR:", appName + " is not installed")
         }
-
     }
 }
