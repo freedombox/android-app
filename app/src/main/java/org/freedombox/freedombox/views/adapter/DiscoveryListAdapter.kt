6e42c0576787746dd9f8d29da8376dfad955e026
abilashr <abilaashsai@gmail.com>
2017-09-04 11:23:29 +0530

    Refactor DiscoveryListAdapter

--- app/src/main/java/org/freedombox/freedombox/views/adapter/DiscoveryListAdapter.kt
+++ app/src/main/java/org/freedombox/freedombox/views/adapter/DiscoveryListAdapter.kt
@@ -25,40 +25,29 @@ import android.widget.BaseAdapter
 import android.widget.TextView
 import org.freedombox.freedombox.R
 
-class DiscoveryListAdapter(val context: Context, boxNameList: ArrayList<String>, portList: ArrayList<String>) : BaseAdapter() {
-
-    private var boxNameList: ArrayList<String>
-    private var portList: ArrayList<String>
-
-    init {
-        this.boxNameList = boxNameList
-        this.portList = portList
-    }
+class DiscoveryListAdapter(val context: Context,
+                           val boxNameList: ArrayList<String>,
+                           val portList: ArrayList<String>) : BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         var inflaterView = convertView
         if (inflaterView == null) {
-            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)
+                    as LayoutInflater
             inflaterView = inflater.inflate(R.layout.discovery_listview, null)
         }
         val boxName = inflaterView?.findViewById(R.id.boxname) as TextView
         val port = inflaterView.findViewById(R.id.port) as TextView
-        boxName.text = boxNameList.get(position)
-        port.text = portList.get(position)
+        boxName.text = boxNameList[position]
+        port.text = portList[position]
         return inflaterView
 
     }
 
-    override fun getItem(position: Int): Any {
-        return position
-    }
+    override fun getItem(position: Int) = boxNameList[position]
 
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
+    override fun getItemId(position: Int) = boxNameList[position].hashCode().toLong()
 
-    override fun getCount(): Int {
-        return boxNameList.size
-    }
+    override fun getCount() = boxNameList.size
 
 }
