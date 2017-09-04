3cfb38e87d1b5b4653672b42dbbd72affbede2a6
abilashr <abilaashsai@gmail.com>
2017-09-04 12:39:30 +0530

    Improve tests for Discovery List Adapter

--- app/src/test/java/org/freedombox/freedombox/view/adapter/DiscoveryListAdapterTest.kt
+++ app/src/test/java/org/freedombox/freedombox/view/adapter/DiscoveryListAdapterTest.kt
@@ -17,10 +17,7 @@
 package org.freedombox.freedombox.view.adapter
 
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
 import org.freedombox.freedombox.BuildConfig
-import org.freedombox.freedombox.views.activities.DiscoveryActivity
 import org.freedombox.freedombox.views.adapter.DiscoveryListAdapter
 import org.junit.Assert
 import org.junit.Before
@@ -29,49 +26,43 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.RuntimeEnvironment
 import org.robolectric.annotation.Config
-import javax.inject.Inject
 
 
 @RunWith(RobolectricTestRunner::class)
 @Config(constants = BuildConfig::class)
 class DiscoveryListAdapterTest {
 
-    @Inject
-    private lateinit var activity: DiscoveryActivity
-
-    @Inject
-    private lateinit var aaa: LayoutInflater
-
-    @Inject
-    private lateinit var vvv: View
-
     private val applicationContext: Context = RuntimeEnvironment.application.applicationContext
-    var boxNameList = ArrayList<String>()
-    var portList = ArrayList<String>()
+    val boxNameList = mutableListOf<String>()
+    val portList = mutableListOf<String>()
     private lateinit var listAdapter: DiscoveryListAdapter
-    var item1 = "item1"
-    var item2 = "item2"
+    val boxName1 = "box1"
+    val boxName2 = "box2"
+    val portName1 = "port1"
+    val portName2 = "port2"
 
     @Before
     fun setUp() {
-        boxNameList.add(item1)
-        portList.add(item2)
+        boxNameList.add(boxName1)
+        portList.add(portName1)
+        boxNameList.add(boxName2)
+        portList.add(portName2)
         listAdapter = DiscoveryListAdapter(applicationContext, boxNameList, portList)
     }
 
     @Test
     fun testItemCount() {
-        Assert.assertEquals(listAdapter.count, 1)
+        Assert.assertEquals(listAdapter.count, 2)
     }
 
     @Test
-    fun testGetItemAtPosition() {
-        Assert.assertEquals(listAdapter.getItem(0), 0)
+    fun testGetItemAtPositionZero() {
+        Assert.assertEquals(listAdapter.getItem(0), boxNameList[0])
     }
 
     @Test
-    fun testGetItemId() {
-        Assert.assertEquals(listAdapter.getItemId(0), 0)
+    fun testGetItemIdAtPositionOne() {
+        Assert.assertEquals(listAdapter.getItemId(1), boxNameList[1].hashCode().toLong())
     }
 
 }
